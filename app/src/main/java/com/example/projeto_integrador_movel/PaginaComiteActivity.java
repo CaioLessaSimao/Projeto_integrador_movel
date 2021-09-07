a5dbbc29488b28c929f29095a17d2249cc051ea3
MountSome <caiolessa02@gmail.com>
2021-09-07 18:08:49 -0300

    Mais coisas feitas

--- app/src/main/java/com/example/projeto_integrador_movel/PaginaComiteActivity.java
+++ app/src/main/java/com/example/projeto_integrador_movel/PaginaComiteActivity.java
@@ -2,13 +2,20 @@ package com.example.projeto_integrador_movel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class pagina_comite extends AppCompatActivity {
+public class PaginaComiteActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pagina_comite);
+    }
+
+    public void onClickEntrar(View view) {
+        Intent i = new Intent(PaginaComiteActivity.this,TelaSimulacaoActivity.class);
+        startActivity(i);
     }
 }
