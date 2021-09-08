0648f34d92911c2f2ff04a10ebe7f9d27404f7db
Gabriel Marinho <gmarinho14@gmail.com>
2021-09-08 20:57:56 -0300

    acabamo pora

--- app/src/main/java/com/example/projeto_integrador_movel/QuestaoActivity.java
+++ app/src/main/java/com/example/projeto_integrador_movel/QuestaoActivity.java
@@ -2,6 +2,7 @@ package com.example.projeto_integrador_movel;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -15,6 +16,9 @@ public class QuestaoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questao);
+
+        Toolbar tbQuestao = findViewById(R.id.tbQuestao);
+        setSupportActionBar(tbQuestao);
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -30,7 +34,7 @@ public class QuestaoActivity extends AppCompatActivity {
         //Isso é um "if"
         switch (item.getItemId()) {
             //Isso é a "condição do if"
-            case R.id.btVoltarMocao:
+            case R.id.btVoltarQuestao:
                 Intent i = new Intent(QuestaoActivity.this, TelaSimulacaoActivity.class);
                 startActivity(i);
                 return true;
