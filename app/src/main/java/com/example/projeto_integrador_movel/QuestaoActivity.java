2fac67c71f43c9cc4fda4c9601f7b238c2ae3318
Gabriel Marinho <gmarinho14@gmail.com>
2021-09-08 20:26:02 -0300

    tela de questao

--- app/src/main/java/com/example/projeto_integrador_movel/QuestaoActivity.java
+++ app/src/main/java/com/example/projeto_integrador_movel/QuestaoActivity.java
@@ -1,8 +1,13 @@
 package com.example.projeto_integrador_movel;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class QuestaoActivity extends AppCompatActivity {
 
@@ -10,5 +15,27 @@ public class QuestaoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questao);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.tbquestao,menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+        //Isso é um "if"
+        switch (item.getItemId()) {
+            //Isso é a "condição do if"
+            case R.id.btVoltarMocao:
+                Intent i = new Intent(QuestaoActivity.this, TelaSimulacaoActivity.class);
+                startActivity(i);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
