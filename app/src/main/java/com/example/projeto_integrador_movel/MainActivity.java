0512f3aa0cee7cdca7bc849cda22d5b46b88c873
MountSome <caiolessa02@gmail.com>
2021-12-09 14:37:19 -0300

    Sla

--- app/src/main/java/com/example/projeto_integrador_movel/MainActivity.java
+++ app/src/main/java/com/example/projeto_integrador_movel/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,10 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(Config.getLogin(MainActivity.this).isEmpty()) {
+            Intent i = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(i);
+            finish();
+        }
+        else {
+            Intent i = new Intent(MainActivity.this, PaginaComiteActivity.class);
+            startActivity(i);
+            finish();
+        }
     }
 
-    public void onClickPaginaComite(View view) {
-        Intent i = new Intent(MainActivity.this,PaginaComiteActivity.class);
-        startActivity(i);
-    }
 }
