f1199adcc25fdbbf40491e91a36344cae691ed99
CaioLessaSimao <caiolessa02@gmail.com>
2022-01-25 10:33:08 -0300

    "finalizado entre aspas"

--- app/src/main/java/com/example/projeto_integrador_movel/TelaSimulacaoActivity.java
+++ app/src/main/java/com/example/projeto_integrador_movel/TelaSimulacaoActivity.java
@@ -10,7 +10,9 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 //TUDO CERTO
@@ -28,6 +30,35 @@ public class TelaSimulacaoActivity extends AppCompatActivity {
         TelaSimulacaoActivityViewModel vm = new ViewModelProvider(this).get(TelaSimulacaoActivityViewModel.class);
         vm.nomeComiteLV.postValue(nomeComite);
         vm.temaComiteLV.postValue(temaComite);
+
+        LiveData<String> nomeComiteLV = vm.getNomeComiteLV();
+        nomeComiteLV.observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+                TextView tvNomCom = findViewById(R.id.tvNomeCom);
+                tvNomCom.setText(s);
+            }
+        });
+
+        LiveData<String> temaComiteLV = vm.getTemaComiteLV();
+        temaComiteLV.observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+                TextView tvTemaCom = findViewById(R.id.tvTemaCom);
+                tvTemaCom.setText(s);
+            }
+        });
+
+        LiveData<String> proximoLV = vm.getProximoLV();
+        proximoLV.observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+                vm.proximoLV.postValue(s);
+                TextView tvPrx = findViewById(R.id.tvPrx);
+                String res = "Discursando agora: " + s;
+                tvPrx.setText(res);
+            }
+        });
 
         RecyclerView rvLstO = findViewById(R.id.rvLstO);
         rvLstO.setHasFixedSize(true);
@@ -52,9 +83,25 @@ public class TelaSimulacaoActivity extends AppCompatActivity {
                 String nomeDel = psgdAction.nomeDel;
                 if(action.equals("adicionar")){
                     //verificar se array esta vazio
-                    List<String> oradores = vm.lstOLV.getValue();
-                    oradores.add(nomeDel);
-                    vm.lstOLV.postValue(oradores);
+                    if(oradoresLV.getValue() == null){
+                        List<String> oradores = new ArrayList<>();
+                        oradores.add(nomeDel);
+                        vm.lstOLV.postValue(oradores);
+                    }
+                    else{
+                        List<String> oradores = vm.lstOLV.getValue();
+                        oradores.add(nomeDel);
+                        vm.lstOLV.postValue(oradores);
+                    }
+                }
+                else if(action.equals("proximo")){
+                    if(oradoresLV.getValue() != null){
+                        List<String> oradores = vm.lstOLV.getValue();
+                        String proximo = nomeDel;
+                        oradores.remove(0);
+                        vm.lstOLV.postValue(oradores);
+                        vm.proximoLV.postValue(proximo);
+                    }
                 }
             }
         });
